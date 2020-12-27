9561bef64111326df2e0793eddab1310c5ad8a17
Mahdi Pishguy <pishguy@gmail.com>
2020-12-27 13:55:03 +0330

    Add files via upload

--- app/Http/Controllers/UserPostController.php
+++ app/Http/Controllers/UserPostController.php
@@ -9,6 +9,6 @@ class UserPostController extends Controller
 {
     public function __invoke(Request $request, User $user)
     {
-        return view('posts.index', ['posts' => $user->posts(), 'user' => $user]);
+        return view('posts.index', ['categories' => $user->availableCategoriesFlattened()->unique('id'), 'user' => $user]);
     }
 }
